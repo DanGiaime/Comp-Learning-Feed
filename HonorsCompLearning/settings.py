43d4d5d0316a4ea46081b1a4e2072f37890ba526
Andy Meyer <itechahm@me.com>
2017-03-01 22:20:13 -0500

    initial views working

--- HonorsCompLearning/settings.py
+++ HonorsCompLearning/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'HonorsCompLearning.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['CompLearningApp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
