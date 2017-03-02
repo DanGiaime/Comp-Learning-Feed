0a272330681c25ee37b898a0634c9a0280cedbec
Dan Giaime <dsg6874@g.rit.edu>
2017-03-01 23:15:01 -0500

    Finished basic event model

--- CompLearningApp/models.py
+++ CompLearningApp/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
+from django.contrib.admin import ModelAdmin
 
 class Event(models.Model):
-    owner_id = models.OneToOneField(User);
-    time = models.DateTimeField();
-    location = models.CharField(max_length=200);
-    description = models.CharField(max_length=200);
-    upload = models.FileField(upload_to='uploads/');
-    attendees = models.ManyToManyField(User);
-    tags = models.ManyToManyField(Tag);
+    owner_id = models.IntegerField()
+    time = models.DateTimeField()
+    location = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    upload = models.FileField(upload_to='uploads/')
+    attendees = models.ManyToManyField(User)
+    #tags = models.ManyToManyField(Tag);
 
+class Provider(User):
+    groups = ['Provider']
+
+class Administrator(User):
+    groups = ['Administrator']
