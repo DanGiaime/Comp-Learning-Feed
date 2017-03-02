2364dcdd2c690b2b2f4869a5c60e5aa42b49d8d9
Dan Giaime <dsg6874@g.rit.edu>
2017-03-01 22:30:16 -0500

    Event model

--- CompLearningApp/models.py
+++ CompLearningApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User, Group
 
-# Create your models here.
+class Event(models.Model):
+    owner_id = models.OneToOneField(User);
+    time = models.DateTimeField();
+    location = models.CharField(max_length=200);
+    description = models.CharField(max_length=200);
+    upload = models.FileField(upload_to='uploads/');
+    attendees = models.ManyToManyField(User);
+    tags = models.ManyToManyField(Tag);
+
